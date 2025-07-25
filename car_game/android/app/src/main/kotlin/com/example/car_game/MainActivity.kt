d94edd20d297fa48452ab8365d8a461fda90a865
Haider Majid <92584858+haider-majid@users.noreply.github.com>
2025-07-25 23:36:09 +0300

    Remove all files related to the car game project, including platform-specific configurations, assets, and source code. This commit effectively cleans up the repository by deleting unnecessary files and directories.

--- car_game/android/app/src/main/kotlin/com/example/car_game/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.car_game
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- car_game/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flame/game.dart';
-import 'game/turbo_track_game.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(GameWidget(game: TurboTrackGame()));
-}
